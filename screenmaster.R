820dd96ca347144c0b40b717c63cf21ccf726d49
Nathan Hausspiegel <nhausspiegel@wesleyan.edu>
2023-11-14 15:59:08 -0500

    moved testing code out of this file to tests.R

--- screenmaster.R
+++ screenmaster.R
@@ -1,5 +1,6 @@
 ## screen master function
 
+source("tests.R")
 
 screen <- function(formula, data, threshold){
 
@@ -9,26 +10,3 @@ screen <- function(formula, data, threshold){
   # statistical tests
 
 }
-
-# install and/or load package
-pacman::p_load(naniar)
-
-# returns vectors of column names that have less than 40% of the data missing
-missing.data <- function(data, var.names, threshold = 0.4) {
-    darwinism <- c()
-    for (var in var.names) {
-        miss <- pct_miss(data[[var]])/100
-        if (miss < threshold) {
-            darwinism <- c(darwinism, var)
-        }
-    }
-    return(darwinism)
-}
-
-#example
-pct_miss(airquality[["Ozone"]])
-pct_miss(airquality[["Solar.R"]])
-
-missing.data(airquality, c("Ozone", "Solar.R"), threshold = 0.2)
-missing.data(airquality, c("Ozone", "Solar.R"), threshold = 0.3)
-missing.data(airquality, c("Ozone", "Solar.R"))
